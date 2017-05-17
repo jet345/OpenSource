682b853f57417dd078a8a7900317221d3abff316
jet345 <rudal1994@naver.com>
2017-05-17 17:33:54 +0900

    Lab11

---
+++ Lab11/array_list.h
@@ -0,0 +1,16 @@
+#pragma once
+// array_list.h
+#include <stddef.h>
+typedef struct _array_list ArrayList;
+ArrayList *newArrayList(size_t);
+void deleteArrayList(ArrayList*);
+struct _array_list {
+	size_t sizeOfElement;
+	size_t numOfElements;
+	void **elements;
+	void(*add)(ArrayList*, void*);
+	void(*insert)(ArrayList*, int, void*);
+	void *(*remove)(ArrayList*, int);
+	size_t(*size)(ArrayList*);
+	void *(*get)(ArrayList*, int);
+};
